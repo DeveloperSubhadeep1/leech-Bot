2f8b1e900ff90b0efc048e66e41fc015ebcdff4d
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2025-11-30 18:41:02 +0530

    Update config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -1,32 +1,149 @@
+# # REQUIRED CONFIG
+# BOT_TOKEN = ""
+# OWNER_ID = 0
+# TELEGRAM_API = 0
+# TELEGRAM_HASH = ""
+# # OPTIONAL CONFIG
+# TG_PROXY = {}
+# USER_SESSION_STRING = ""
+# CMD_SUFFIX = ""
+# AUTHORIZED_CHATS = ""
+# SUDO_USERS = ""
+# DATABASE_URL = ""
+# STATUS_LIMIT = 4
+# DEFAULT_UPLOAD = "rc"
+# STATUS_UPDATE_INTERVAL = 15
+# FILELION_API = ""
+# STREAMWISH_API = ""
+# EXCLUDED_EXTENSIONS = ""
+# INCOMPLETE_TASK_NOTIFIER = False
+# YT_DLP_OPTIONS = ""
+# USE_SERVICE_ACCOUNTS = False
+# NAME_SUBSTITUTE = ""
+# FFMPEG_CMDS = {}
+# UPLOAD_PATHS = {}
+# # GDrive Tools
+# GDRIVE_ID = ""
+# IS_TEAM_DRIVE = False
+# STOP_DUPLICATE = False
+# INDEX_URL = ""
+# # Rclone
+# RCLONE_PATH = ""
+# RCLONE_FLAGS = ""
+# RCLONE_SERVE_URL = ""
+# RCLONE_SERVE_PORT = 0
+# RCLONE_SERVE_USER = ""
+# RCLONE_SERVE_PASS = ""
+# # JDownloader
+# JD_EMAIL = ""
+# JD_PASS = ""
+# # Sabnzbd
+# USENET_SERVERS = [
+#     {
+#         "name": "main",
+#         "host": "",
+#         "port": 563,
+#         "timeout": 60,
+#         "username": "",
+#         "password": "",
+#         "connections": 8,
+#         "ssl": 1,
+#         "ssl_verify": 2,
+#         "ssl_ciphers": "",
+#         "enable": 1,
+#         "required": 0,
+#         "optional": 0,
+#         "retention": 0,
+#         "send_group": 0,
+#         "priority": 0,
+#     }
+# ]
+# # Nzb search
+# HYDRA_IP = ""
+# HYDRA_API_KEY = ""
+# # Update
+# UPSTREAM_REPO = ""
+# UPSTREAM_BRANCH = "master"
+# # Leech
+# LEECH_SPLIT_SIZE = 0
+# AS_DOCUMENT = False
+# EQUAL_SPLITS = False
+# MEDIA_GROUP = False
+# USER_TRANSMISSION = False
+# HYBRID_LEECH = False
+# LEECH_FILENAME_PREFIX = ""
+# LEECH_DUMP_CHAT = ""
+# THUMBNAIL_LAYOUT = ""
+# # qBittorrent/Aria2c
+# TORRENT_TIMEOUT = 0
+# BASE_URL = ""
+# BASE_URL_PORT = 0
+# WEB_PINCODE = False
+# # Queueing system
+# QUEUE_ALL = 0
+# QUEUE_DOWNLOAD = 0
+# QUEUE_UPLOAD = 0
+# # RSS
+# RSS_DELAY = 600
+# RSS_CHAT = ""
+# RSS_SIZE_LIMIT = 0
+# # Torrent Search
+# SEARCH_API_LINK = ""
+# SEARCH_LIMIT = 0
+# SEARCH_PLUGINS = [
+#     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+#     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
+#     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torlock.py",
+#     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+#     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
+#     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentproject.py",
+#     "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
+#     "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
+#     "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
+#     "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/nyaasi.py",
+#     "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
+#     "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
+#     "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
+#     "https://raw.githubusercontent.com/v1k45/1337x-qBittorrent-search-plugin/master/leetx.py",
+#     "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
+#     "https://raw.githubusercontent.com/msagca/qbittorrent_plugins/main/uniondht.py",
+#     "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py",
+# ]
+
+
+
 # REQUIRED CONFIG
-BOT_TOKEN = ""
-OWNER_ID = 0
-TELEGRAM_API = 0
-TELEGRAM_HASH = ""
+BOT_TOKEN = "7203772931:AAFw3mgaT9Sx_F6ByiljqComBAG25mSh0AA"
+OWNER_ID = 6075512585
+TELEGRAM_API = 27972068
+TELEGRAM_HASH = "6e7e2f5cdddba536b8e603b3155223c1"
+
 # OPTIONAL CONFIG
 TG_PROXY = {}
 USER_SESSION_STRING = ""
 CMD_SUFFIX = ""
-AUTHORIZED_CHATS = ""
-SUDO_USERS = ""
-DATABASE_URL = ""
+AUTHORIZED_CHATS = "-5072761192"
+SUDO_USERS = "6075512585"
+DATABASE_URL = "mongodb+srv://hellking1:hellking1@cluster0.zqg7rco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 STATUS_LIMIT = 4
 DEFAULT_UPLOAD = "rc"
-STATUS_UPDATE_INTERVAL = 15
+STATUS_UPDATE_INTERVAL = 10
 FILELION_API = ""
 STREAMWISH_API = ""
 EXCLUDED_EXTENSIONS = ""
-INCOMPLETE_TASK_NOTIFIER = False
+INCOMPLETE_TASK_NOTIFIER = True
 YT_DLP_OPTIONS = ""
 USE_SERVICE_ACCOUNTS = False
 NAME_SUBSTITUTE = ""
 FFMPEG_CMDS = {}
 UPLOAD_PATHS = {}
+
 # GDrive Tools
 GDRIVE_ID = ""
 IS_TEAM_DRIVE = False
 STOP_DUPLICATE = False
 INDEX_URL = ""
+
 # Rclone
 RCLONE_PATH = ""
 RCLONE_FLAGS = ""
@@ -34,9 +151,11 @@ RCLONE_SERVE_URL = ""
 RCLONE_SERVE_PORT = 0
 RCLONE_SERVE_USER = ""
 RCLONE_SERVE_PASS = ""
+
 # JDownloader
-JD_EMAIL = ""
-JD_PASS = ""
+JD_EMAIL = "fikeh71516@idwager.com"
+JD_PASS = "6!nNQgc5x*gyWCa"
+
 # Sabnzbd
 USENET_SERVERS = [
     {
@@ -58,12 +177,15 @@ USENET_SERVERS = [
         "priority": 0,
     }
 ]
+
 # Nzb search
 HYDRA_IP = ""
 HYDRA_API_KEY = ""
+
 # Update
 UPSTREAM_REPO = ""
 UPSTREAM_BRANCH = "master"
+
 # Leech
 LEECH_SPLIT_SIZE = 0
 AS_DOCUMENT = False
@@ -74,19 +196,23 @@ HYBRID_LEECH = False
 LEECH_FILENAME_PREFIX = ""
 LEECH_DUMP_CHAT = ""
 THUMBNAIL_LAYOUT = ""
+
 # qBittorrent/Aria2c
-TORRENT_TIMEOUT = 0
+TORRENT_TIMEOUT = 1000
 BASE_URL = ""
-BASE_URL_PORT = 0
+BASE_URL_PORT = 8080
 WEB_PINCODE = False
+
 # Queueing system
 QUEUE_ALL = 0
 QUEUE_DOWNLOAD = 0
 QUEUE_UPLOAD = 0
+
 # RSS
 RSS_DELAY = 600
 RSS_CHAT = ""
 RSS_SIZE_LIMIT = 0
+
 # Torrent Search
 SEARCH_API_LINK = ""
 SEARCH_LIMIT = 0
